5e21fbe07c4cd3e96ede81bd7fc01f172f4b1e58
anamg <anamigar.96@gmail.com>
2025-02-09 21:00:33 +0100

    codificación_datos

--- original/Part 1/DataPreprocessing/ANA_data_preprocessing_temp.py
+++ original/Part 1/DataPreprocessing/ANA_data_preprocessing_temp.py
@@ -16,4 +16,21 @@ imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
 imputer = imputer.fit(X[:,1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-print(X)
+#codificar datos categóricos de la matriz X
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+
+#define qué columnas son categóricas (posición 0 del array) - transforma en variable dummy
+from sklearn.compose import ColumnTransformer
+ct = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [0])], remainder="passthrough")
+X_encoded = ct.fit_transform(X)
+
+
+#codificar datos categóricos del array Y
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+#DIvidir el data set en conjto de entrenamiento y de testing
+from sklearn.model_selection import train_test_split
+X_encoded_train, X_encoded_test, y_trains, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=0)
